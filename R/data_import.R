be0d267d7f0e510a164713b10c05393933b5e1cb
Isaac M Bazian <bazia001@umn.edu>
2023-05-02 09:49:31 -0500

    Each dataset imported and cleaned, will attempt join

--- R/data_import.R
+++ R/data_import.R
@@ -9,10 +9,8 @@ employee_data_tbl <- read_delim(file = "../data/dataset.csv", delim = "+") %>%
   mutate(employee_id = row_number())
 
 satisfaction_tbl <- read_delim(file = "../data/satisfaction_reviews.csv", delim = "@!?", col_names = "satisfaction") %>% 
-  mutate(employee_id = as.integer(str_match(string = satisfaction, pattern = "\\.(\\d+)$")[,2]))
+  mutate(employee_id = as.integer(str_extract(string = satisfaction, pattern = "(\\d+)$"))) %>% 
+  mutate(satisfaction = str_remove(string = satisfaction, pattern = "(\\d+)$"))
 
-
-
-str_remove(string = satisfaction_tbl$satisfaction, pattern = "\\.(\\d+)$")
 
 
