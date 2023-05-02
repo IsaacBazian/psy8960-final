c9128707fc2f388f5221b74054dcd50aecc92944
Isaac M Bazian <bazia001@umn.edu>
2023-05-02 10:11:41 -0500

    Done with initial data import and cleaning, saved to RDS file

--- R/data_import.R
+++ R/data_import.R
@@ -10,7 +10,11 @@ employee_data_tbl <- read_delim(file = "../data/dataset.csv", delim = "+") %>%
 
 satisfaction_tbl <- read_delim(file = "../data/satisfaction_reviews.csv", delim = "@!?", col_names = "satisfaction") %>% 
   mutate(employee_id = as.integer(str_extract(string = satisfaction, pattern = "(\\d+)$"))) %>% 
-  mutate(satisfaction = str_remove(string = satisfaction, pattern = "(\\d+)$"))
+  mutate(satisfaction = str_remove(string = satisfaction, pattern = "(\\d+)$")) %>% 
+  mutate(satisfaction = str_remove_all(string = satisfaction, pattern = "NA\\.")) %>% 
+  mutate(satisfaction = na_if(satisfaction, ""))
 
+finalproj_tbl <- left_join(employee_data_tbl, satisfaction_tbl, join_by(employee_id))
 
+saveRDS(finalproj_tbl, "../data/finalproj_data.rds")
 
