5d3532976fe270c0528f563bc9ab4ec48836314d
Isaac M Bazian <bazia001@umn.edu>
2023-05-02 09:42:42 -0500

    Working on data import and cleaning

--- R/data_import.R
+++ R/data_import.R
@@ -3,4 +3,16 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 
+# Data Import and Cleaning
+
+employee_data_tbl <- read_delim(file = "../data/dataset.csv", delim = "+") %>% 
+  mutate(employee_id = row_number())
+
+satisfaction_tbl <- read_delim(file = "../data/satisfaction_reviews.csv", delim = "@!?", col_names = "satisfaction") %>% 
+  mutate(employee_id = as.integer(str_match(string = satisfaction, pattern = "\\.(\\d+)$")[,2]))
+
+
+
+str_remove(string = satisfaction_tbl$satisfaction, pattern = "\\.(\\d+)$")
+
 
