fdd58a7cbcb1bce06bfa6928c0a7bf716b75820d
Isaac M Bazian <bazia001@umn.edu>
2023-05-06 18:06:41 -0500

    Rough tibble and plot for hypothesis 1 done

--- R/statistical.R
+++ R/statistical.R
@@ -7,3 +7,28 @@ library(rstatix)
 finalproj_stats_tbl <- readRDS("../data/finalproj_data.rds")
 
 
+# Analysis
+
+H1_tbl <- finalproj_stats_tbl %>% 
+  cor_test("MonthlyIncome", "PerformanceRating", alternative = "two.sided", method = "pearson", conf.level = 0.95) #All defaults, but it informs reader
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Visualization
+
+ggplot(finalproj_stats_tbl, aes(x = PerformanceRating, y = MonthlyIncome)) + 
+  geom_point(position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
