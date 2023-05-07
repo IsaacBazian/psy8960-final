b65f5e48c1c8e6f47c263d2bef8e46b965bfb379
Isaac M Bazian <bazia001@umn.edu>
2023-05-07 10:38:18 -0500

    Saved my initial figs, need to make sure I have everything for publication and the write that section

--- R/statistical.R
+++ R/statistical.R
@@ -24,11 +24,14 @@ H2_tukeyhsd_tbl <- finalproj_stats_tbl %>%
   tukey_hsd(MonthlyIncome ~ Department)
 
 
+
+
+
 # Hypothesis 3
 
 H3_model <- lm(YearsAtCompany ~ RelationshipSatisfaction*Gender, data = finalproj_stats_tbl)
-summary(H3_model)
-
+H3_list <- summary(H3_model)
+H3_list
 
 
 
@@ -45,21 +48,33 @@ H1_plot <- ggplot(finalproj_stats_tbl, aes(x = PerformanceRating, y = MonthlyInc
 
 H1_plot
 
-
+ggsave("../figs/H1.png", plot = H1_plot, width = 8, height = 4.5, units = "in", dpi = 300)
 
 # Visualization of H2
-ggplot(finalproj_stats_tbl, aes(x = Department, y = MonthlyIncome)) +
+H2_plot <- ggplot(finalproj_stats_tbl, aes(x = Department, y = MonthlyIncome)) +
   geom_boxplot() +
   labs(y = "Monthly Income")
 
+H2_plot
 
+ggsave("../figs/H2.png", plot = H2_plot, width = 8, height = 4.5, units = "in", dpi = 300)
 
 # Visualization of H3
-ggplot(finalproj_stats_tbl, aes(x = predict(H3_model), y = RelationshipSatisfaction, color = Gender, group = Gender)) +
-  geom_point(position = position_jitter()) +
+H3_plot <- ggplot(finalproj_stats_tbl, aes(x = predict(H3_model), y = RelationshipSatisfaction, color = Gender, group = Gender)) +
+  geom_point(size = 0.5, position = position_jitter()) +
   geom_smooth(method = "lm", se = F) +
   labs(x = "Predicted Years at Company", y = "Relationship Satisfaction")
 
+H3_plot
+
+ggsave("../figs/H3.png", plot = H3_plot, width = 8, height = 4.5, units = "in", dpi = 300)
+
+
+
+
+# Publication
+
+# Publication Results for H1
 
 
 
